af063aafe44ad47d095988d96091363e28fc012c
Abdoulaye Aliou SALL <87236495+babagl@users.noreply.github.com>
2021-11-02 19:58:15 +0000

    Add files via upload

---
+++ CV/js/script.js
@@ -0,0 +1,16 @@
+
+
+
+/*--------Bg Animation effect------------*/
+function bgAnimationItems() {
+   const rows = 7, cols = 10;
+   for(let i = 0; i< rows; i++){
+       for (let j = 0; j < cols; j++) {
+           const div = document.createElement("div");
+           div.className = 'col-${j+1}';
+           document.querySelector(".bg-animation-effect").appendChild(div);
+           
+       }
+   }
+}
+bgAnimationItems();
